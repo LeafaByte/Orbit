18e337f1b7a054db502ccfb57e3741cdf453a18a
Sana <thexsa404e3x@gmail.com>
2026-07-02 14:59:39 +0330

    Search app- final

--- backend/soft982.py
+++ backend/soft982.py
@@ -1,7 +1,6 @@
 from playwright.sync_api import sync_playwright
 from jelo_profiler import Profiler
 t = Profiler()
-
 def open_browser():
     p = sync_playwright().start()
     Browser = p.chromium.launch(headless=False)
@@ -12,8 +11,8 @@ def open_browser():
     
     return Browser , page , p
 
-def search_in_site(page, query: str):
-    page.goto("https://soft98.ir/?do=search", wait_until="domcontentloaded")
+def search_in_site(Link, page, query: str):
+    page.goto(Link, wait_until="domcontentloaded")
     page.fill("#searchinput",query)
     page.keyboard.press("Enter")
     page.wait_for_selector(".cbddtl", state="visible")
@@ -24,39 +23,54 @@ def search_in_site(page, query: str):
 def extract_results (search_items):
     results = []
     for i in range(search_items.count()):
-
         item = search_items.nth(i)
-
         title = item.inner_text()
-
         url = item.get_attribute("href")
-
         results.append(
             {
                 "title": title,
                 "url": url
             }
         )
-
+    for i, item in enumerate(results, start=1):
+            print(f"{i}. {item['title']}")
     return results
+
+def select_app(results):
+    choice = int(input("Choose app: "))
+    selected_app = results[choice - 1]
+    return selected_app
+
+def open_app_page(page, selected_app):
+    
+    page.goto(
+        selected_app["url"],
+        wait_until="domcontentloaded"
+    )
 
     
 def main():
     query = input("Enter app name: ")
     Browser , page , p = open_browser()
-    search_items = search_in_site(page, query)
+    search_items = search_in_site("https://soft98.ir/?do=search",page, query)
+   
     
     try:
         print("Search finished — page loaded")
-        page.wait_for_timeout(3000)
+        # page.wait_for_timeout(3000)
         results = extract_results(search_items)
-        for result in results:
-            print(result)
+        selected_app = select_app(results)
 
+        print("\nSelected App:")
+        print(selected_app["title"])
+        print(selected_app["url"])
+        open_app_page(page, selected_app)
+        if page.url == selected_app["url"]:
+            print("successful!")
+
+         
     finally:
         Browser.close()
         p.stop()
         
 main()
-    
-
